5cd9dfc9182a4105379eda26e055b2f314d72bf4
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-15 16:13:49 -0400

    Removed old GetComputerList.cs comment header

--- GetComputerList.cs
+++ GetComputerList.cs
@@ -1,8 +1,4 @@
-﻿//****************************************
-// Copyright (c) SystemsAdminPro 2012
-//****************************************
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
